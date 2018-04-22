b14bd1e9c9ea22f3fba32da75e707a714f06673f
anuraagmoturi <anuraagmoturivb@gmail.com>
2018-04-22 16:01:29 -0400

    using callbacks, promises

---
+++ weather/weather.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+  request({
+    url: `https://api.darksky.net/forecast/f4628a0282b99efb57994a3c06af2ef8/${lat},${lng}`,
+    json: true
+  }, (error, response, body)=>{
+    if(!error && response.statusCode === 200) {
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
+    }
+    else{
+      callback('Unable to fetch weather');
+    }
+  });
+};
+
+module.exports = {getWeather};
